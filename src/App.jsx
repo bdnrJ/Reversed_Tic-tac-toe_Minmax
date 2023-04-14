795665d1c5d8098928f86ffb4faabd2acc13d7d3
bdnrJ <jbednarczyk001@gmail.com>
2023-04-14 11:38:26 +0200

    first maybe last commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import TicTacToe from './TicTacToe'
+
+function App() {
+
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App
